a4fb2cd7cddb130ee8082aada9cfe13fd83c794f
julioojordan <andyanjordan1153@gmail.com>
2024-08-26 19:27:05 +0700

    WIP phase 1 dashboard - add query helper for th outcome and income if thereis wilayah and lingkungan

--- repository/transactionHistoryRepositoryImpl.go
+++ repository/transactionHistoryRepositoryImpl.go
@@ -9,11 +9,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type THRequest struct {
-	IdWilayah string `json:"id_wilayah"`
-	IdLingkungan string `json:"id_lingkungan"`
-}
-
 type transactionHistoryRepositoryImpl struct {
 }
 
@@ -22,25 +17,30 @@ func NewTransactionHistoryRepository(db *sql.DB) TransactionHistoryRepository {
 }
 
 func (repository *transactionHistoryRepositoryImpl) GetTotalIncome(ctx *fiber.Ctx, tx *sql.Tx) (entity.AmountHistory, error) {
-	sqlScript := "SELECT SUM(nominal) FROM riwayat_transaksi WHERE keterangan = 'IN'"
-	result, err :=tx.Query(sqlScript)
-	helper.PanicIfError(err);
-	defer result.Close()
-	fmt.Println("total income", result);
-	
-	AmountHistory := entity.AmountHistory{}
-	if result.Next(){
-		err := result.Scan(&AmountHistory.Nominal)
-		helper.PanicIfError(err)
-		return AmountHistory, nil
-	} else{
-		return AmountHistory, fiber.NewError(fiber.StatusInternalServerError , "Error Internal")
-	}
+    idWilayah := ctx.Query("id_wilayah")
+    idLingkungan := ctx.Query("id_lingkungan")
+    sqlScript := "SELECT SUM(nominal) FROM riwayat_transaksi WHERE keterangan = 'IN'"
+	result, err := helper.AddLingkunganOrWilayahQueryHelper(idWilayah, idLingkungan, sqlScript, tx);
+    helper.PanicIfError(err)
+    defer result.Close()
+
+    // Proses hasil query
+    AmountHistory := entity.AmountHistory{}
+    if result.Next(){
+        err := result.Scan(&AmountHistory.Nominal)
+        helper.PanicIfError(err)
+        return AmountHistory, nil
+    } else{
+        return AmountHistory, fiber.NewError(fiber.StatusInternalServerError, "Error Internal")
+    }
 }
 
 func (repository *transactionHistoryRepositoryImpl) GetTotalOutcome(ctx *fiber.Ctx, tx *sql.Tx) (entity.AmountHistory, error) {
+	idWilayah := ctx.Query("id_wilayah")
+    idLingkungan := ctx.Query("id_lingkungan")
 	sqlScript := "SELECT SUM(nominal) FROM riwayat_transaksi WHERE keterangan = 'OUT'"
-	result, err :=tx.Query(sqlScript)
+	result, err := helper.AddLingkunganOrWilayahQueryHelper(idWilayah, idLingkungan, sqlScript, tx);
+
 	helper.PanicIfError(err);
 	defer result.Close()
 	fmt.Println("total outcome", result);
